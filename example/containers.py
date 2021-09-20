69f62cad7e9e8bf4a47c81d46a9bd1f843674aa1
Rajesh Babu <Rajesh.Babu@spark.co.nz>
2021-09-20 19:25:06 +1200

    Completed containers & services

--- example/containers.py
+++ example/containers.py
@@ -7,3 +7,44 @@ from dependency_injector import containers, providers
 
 import services
 
+class Container(containers.DeclarativeContainer):
+
+    config = providers.Configuration()
+
+    logging = providers.Resource(
+        logging.config.fileConfig,
+        fname = 'logging.ini'
+    )
+    
+    # Gateways
+
+    database_client = providers.Singleton(
+        sqlite3.connect,
+        config.database.dsn
+    ) 
+
+    s3_client = providers.Singleton(
+        boto3.client,
+        service_name = 's3',
+        aws_access_key = config.aws.access_key_id,
+        aws_secret_access_key = config.aws.aws_secret_access_key,
+    )
+
+    # Services
+
+    user_service = providers.Factory(
+        services.UserService,
+        db = database_client, 
+    )
+
+    auth_service = providers.Factory(
+        services.AuthService,
+        db = database_client,
+        token_ttl = config.auth.token_ttl.as_int(),
+    )
+
+    photo_service = providers.Factory(
+        services.PhotoService,
+        db = database_client,
+        s3 = s3_client,
+    )
--- example/services.py
+++ example/services.py
@@ -21,3 +21,32 @@ class UserService(BaseService):
         self.logger.info(f"User {email} has been found in the database")
 
         return {'email': email, 'password_hash': '...'}
+
+class AuthService(BaseService):
+
+    def __Init__(self, db:sqlite3.Connection, token_ttl: int)-> None:
+        self.db = db
+        self.token_ttl = token_ttl
+        super().__init__()
+
+        def authenticate(self, user:Dict[str,str], password: str)-> None:
+            assert password is not None
+            self.logger.debug('User %s has been successfully authenticated',user['email'],)
+
+
+class PhotoService(BaseService):
+
+    def __init__(self, db: sqlite3.Connection, s3: S3Client)-> None:
+        self.db = db
+        self.s3 = s3
+        super().__init__()
+
+    def upload_photo(self,user: Dict[str, str], photo_path: str)-> None:
+        self.logger.debug(
+            'Photo %s has been successfully uploaded by user %s',
+            photo_path,
+            user['email'],
+        )
+
+
+
